d3b4b7cb8161dc1c2ae5f3953128743c96e3599a
G-type <162748908+gtref@users.noreply.github.com>
2026-09-16 16:13:24 +1200

    Finnish pyright implamentation.

--- preload.js
+++ preload.js
@@ -8,13 +8,27 @@ contextBridge.exposeInMainWorld('api', {
   saveFile: (data) => ipcRenderer.invoke('file:save', data),
   listDirectory: (directoryPath) => ipcRenderer.invoke('directory:list', directoryPath),
   openFolder: () => ipcRenderer.invoke('dialog:openFolder'),
+
   updateCodebaseIndex: (documentId, text) => indexer.updateActiveDocument(documentId, text),
   queryCodebaseGhostText: (prefix) => indexer.queryGhostText(prefix),
+
   exit: () => ipcRenderer.invoke('app:exit'),
+
+  // --- CLANGD ---
   startClangd: (rootPath) => ipcRenderer.invoke('clangd:start', rootPath),
   sendClangdMessage: (message) => ipcRenderer.send('clangd:message', message),
   stopClangd: () => ipcRenderer.send('clangd:stop'),
   onClangdMessage: (callback) => ipcRenderer.on('clangd:message', (_event, message) => callback(message)),
   onClangdStderr: (callback) => ipcRenderer.on('clangd:stderr', (_event, message) => callback(message)),
-  onClangdError: (callback) => ipcRenderer.on('clangd:error', (_event, message) => callback(message))
+  onClangdError: (callback) => ipcRenderer.on('clangd:error', (_event, message) => callback(message)),
+
+  // --- PYRIGHT ---
+  startPyright: (rootPath) => ipcRenderer.invoke('pyright:start', rootPath),
+  sendPyrightMessage: (message) => ipcRenderer.send('pyright:message', message),
+  stopPyright: () => ipcRenderer.send('pyright:stop'),
+  onPyrightMessage: (callback) => ipcRenderer.on('pyright:message', (_event, message) => callback(message)),
+  onPyrightStderr: (callback) => ipcRenderer.on('pyright:stderr', (_event, message) => callback(message)),
+  onPyrightError: (callback) => ipcRenderer.on('pyright:error', (_event, message) => callback(message)),
+  onPyrightExit: (callback) => ipcRenderer.on('pyright:exit', (_event) => callback())
 });
+
