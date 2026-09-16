12f2a9df733859b3c4c44739c890d7b9f25d3e54
G-type <162748908+gtref@users.noreply.github.com>
2026-09-16 20:57:11 +1200

    Add some basic stubs.
    Signed-off-by: G-type <162748908+gtref@users.noreply.github.com>

---
+++ UI/git_sidebar/gitstub.js
@@ -0,0 +1,37 @@
+class StatusStub {
+    static parse(raw) {
+        if (!raw.trim()) {
+            return [];
+        }
+
+        return raw.split("\n").map(line => {
+            const code = line.slice(0, 2).trim();
+            const file = line.slice(2).trim();
+            return { code, file };
+        });
+    }
+}
+
+class BranchStub {
+    static parse(raw) {
+        const lines = raw.split("\n").filter(Bolean);
+
+        let curr = null;
+        const branches = [];
+
+        for (const line of lines) {
+            if (line.startsWith("*")) {
+                current = line.replace("*", "").trim();
+                branches.push(current);
+            } else {
+                branches.push(line.trim());
+            }
+        }
+        return { current, branches };
+    }
+}
+
+module.exports = {
+    StatusStub,
+    BranchStub
+}
